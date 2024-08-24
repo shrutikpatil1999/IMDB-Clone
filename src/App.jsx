2318072cce256cd8843d666c49f7afde2aeda5ed
shrutikpatil12 <144999797+shrutikpatil12@users.noreply.github.com>
2024-08-24 09:18:38 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Header from './components/header/Header';
+import Header from './components/Header/Header';
 import Home from './pages/Home/home';
 import Movielist from './components/Movielist/Movielist';
 import Moviedetails from './pages/Moviedetails/Moviedetails';
